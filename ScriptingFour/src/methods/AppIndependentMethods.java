6e22e43c5bd9a5250dd41ec1b4324e449e21571f
sgTester <gopal.gudi@gmail.com>
2026-07-15 08:32:05 +0530

    added new changes

--- ScriptingFour/src/methods/AppIndependentMethods.java
+++ ScriptingFour/src/methods/AppIndependentMethods.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-
 import java.io.FileInputStream;
 import java.util.*;
 
